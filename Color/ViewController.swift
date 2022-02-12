fe4eb04a48afdd2cb04278e4392e6d80326d162a
Teamiha <jegnum@gmail.com>
2022-02-12 19:58:09 +0300

    codingPrepeare

--- Color/ViewController.swift
+++ Color/ViewController.swift
@@ -10,18 +10,29 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var redMeaning: UILabel!
+    @IBOutlet weak var greenMeaning: UILabel!
+    @IBOutlet weak var blueMeaning: UILabel!
+    
     
     @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
     @IBAction func redSliderAction() {
         let redSliderValue = String(format: "%.2f", redSlider.value)
         redMeaning.text = redSliderValue
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    
+    @IBAction func blueSliderAction() {
     }
     
     
