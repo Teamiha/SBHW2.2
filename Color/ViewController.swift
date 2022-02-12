f429f50e640a63c7c5a6e0f592b000c1e6ff563c
Teamiha <jegnum@gmail.com>
2022-02-12 16:40:34 +0300

    sliderMechanicIsUndestend

--- Color/ViewController.swift
+++ Color/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redMeaning: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func redSliderAction() {
+        let redSliderValue = String(format: "%.2f", redSlider.value)
+        redMeaning.text = redSliderValue
+    }
+    
+    
 }
 
